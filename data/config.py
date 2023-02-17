46fc983ab1848240ab68f71aea2f5982789f4f4b
pavkul <kulpv@mail.ru>
2023-02-17 21:33:13 +0300

    update db requests

--- data/config.py
+++ data/config.py
@@ -1,6 +1,9 @@
 from environs import Env
 import os
 
+from environs import Env
+import os
+
 # Теперь используем вместо библиотеки python-dotenv библиотеку environs
 env = Env()
 env.read_env()
--- utils/sql_queries.py
+++ utils/sql_queries.py
@@ -1,86 +1,75 @@
 create_tables_query = """
 CREATE TABLE IF NOT EXISTS users (
-    id BIGSERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     user_id INTEGER,
     first_name VARCHAR(50),
     last_name VARCHAR(50)
 );
 
 CREATE TABLE IF NOT EXISTS owners (
-    id BIGSERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     first_name VARCHAR(50),
     last_name VARCHAR(50)
 );
 
 CREATE TABLE IF NOT EXISTS streets (
-    id BIGSERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(250)
 );
 
 CREATE TABLE IF NOT EXISTS address (
-    id BIGSERIAL PRIMARY KEY,
-    owner_id INTEGER,
-    street_id INTEGER,
+    id SERIAL PRIMARY KEY,
+    owner_id INTEGER REFERENCES owners(id) ON DELETE CASCADE,
+    street_id INTEGER REFERENCES streets(id) ON DELETE CASCADE,
     house VARCHAR(10),
-    flat INTEGER,
-    FOREIGN KEY (owner_id) REFERENCES owners(id) ON DELETE CASCADE,
-    FOREIGN KEY (street_id) REFERENCES streets(id) ON DELETE CASCADE 
+    flat INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS species (
-    id BIGSERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(50)
 );
 
 CREATE TABLE IF NOT EXISTS breeds (
-    id BIGSERIAL PRIMARY KEY,
-    species_id INTEGER,
-    name VARCHAR(50),
-    FOREIGN KEY (species_id) REFERENCES species(id) ON DELETE CASCADE
+    id SERIAL PRIMARY KEY,
+    species_id INTEGER REFERENCES species(id) ON DELETE CASCADE,
+    name VARCHAR(50)
 );
 
 CREATE TABLE IF NOT EXISTS patients (
-    id BIGSERIAL PRIMARY KEY,
-    species_id INTEGER,
-    breed_id INTEGER,
+    id SERIAL PRIMARY KEY,
+    species_id INTEGER REFERENCES species(id) ON DELETE CASCADE,
+    breed_id INTEGER REFERENCES breeds(id) ON DELETE CASCADE,
     sex VARCHAR(1),
     date_of_birth VARCHAR(20),
     nickname VARCHAR(50), 
-    owner_id INTEGER,
-    FOREIGN KEY (species_id) REFERENCES species(id) ON DELETE CASCADE,
-    FOREIGN KEY (breed_id) REFERENCES breeds(id) ON DELETE CASCADE,
-    FOREIGN KEY (owner_id) REFERENCES owners(id) ON DELETE CASCADE 
+    owner_id INTEGER REFERENCES owners(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS orders (
-    id BIGSERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     date VARCHAR(20),
-    owner_id INTEGER,
+    owner_id INTEGER REFERENCES owners(id) ON DELETE CASCADE,
     patient_id INTEGER,
-    cost INTEGER,
-    FOREIGN KEY (owner_id) REFERENCES owners(id) ON DELETE CASCADE,
-    FOREIGN KEY (patient_id) REFERENCES patients(id) ON DELETE CASCADE 
+    cost INTEGER REFERENCES patients(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS vet_service (
-    id BIGSERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     service VARCHAR(200),
     price INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS order_service (
-    id BIGSERIAL PRIMARY KEY,
-    order_id INTEGER,
-    service_id INTEGER,
-    FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE,
-    FOREIGN KEY (service_id) REFERENCES vet_service(id) ON DELETE CASCADE 
+    id SERIAL PRIMARY KEY,
+    order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
+    service_id INTEGER REFERENCES vet_service(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS order_service (
-    id BIGSERIAL PRIMARY KEY,
-    order_id INTEGER,
-    medication TEXT,
-    FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE
+    id SERIAL PRIMARY KEY,
+    order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
+    medication TEXT
 );
 """
 
